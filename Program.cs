1369a8f8f3ef1e84633cdc7346fcfde8beec3f17
Davi de Castro Oliveira <davicastrooliveira@outlook.com>
2025-03-31 09:03:38 -0300

    Programa que calcula o FGTS sobre o salário de um funcionário

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double salario;
+double FGTS;
+Console.WriteLine("Calcule seu FGTS");
+
+Console.Write("Digite o número do seu salário: ");
+salario = Convert.ToDouble(Console.ReadLine()!);
+Console.Clear();
+
+Console.WriteLine($"Seu salário (R$): {salario}");
+FGTS = salario * 8 / 100;
+Console.WriteLine($"Valor do FGTS: {FGTS}");
