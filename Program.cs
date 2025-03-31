6951b4aa895d127a6acb4df8a23ad5067c0f2cf4
Davi Oliveira <davicastrooliveira@outlook.com>
2025-03-31 09:14:48 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,6 @@ Console.Write("Digite o número do seu salário: ");
 salario = Convert.ToDecimal(Console.ReadLine()!);
 Console.Clear();
 
-Console.WriteLine($"Seu salário (R$): {salario}");
+Console.WriteLine($"Seu salário (R$): {salario:C2}");
 FGTS = salario * 8 / 100;
-Console.WriteLine($"Valor do FGTS: {FGTS}");
+Console.WriteLine($"Valor do FGTS: {FGTS:C2}");
